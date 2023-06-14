e01fc91c5ad5ed3c0386d482d3f0c5c2ed688fdd
char1ey <ckstn410@gmail.com>
2023-06-14 13:01:55 +0900

    Reset: 학습 리셋

--- nest_study/interface/wallet.d.ts
+++
@@ -1,6 +0,0 @@
-export type nickName = string
-export type privateKey = string
-export type publicKey = string
-export type acccount = string
-export type userId = string
-export type userPassword = string
