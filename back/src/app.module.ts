0c5fc6a6ff798b743e85c7b041223cc97c45dcdc
char1ey <ckstn410@gmail.com>
2023-06-15 09:19:38 +0900

    feat:mongoose 연결

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from "@nestjs/config";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from "./config/configuration";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [configuration]
+  }), MongooseModule.forRoot(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {}  
--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from "@nestjs/config";
+
+interface DBConfing {
+  host: string
+  port: string
+}
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService){}
   getHello(): string {
+    console.log(this.configService.get<DBConfing>('database'))
+    console.log(this.configService.get('PORT', { infer: true }))
     return 'Hello World!';
   }
 }
