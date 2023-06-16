896bf4774e13aa4358475c7b48e9d99f680f0d10
char1ey <ckstn410@gmail.com>
2023-06-16 15:14:52 +0900

    feat: nest_multer S3 세팅

--- back/src/file/file.service.ts
+++ back/src/file/file.service.ts
@@ -7,6 +7,12 @@ export class FileService {
         if(!file) throw new BadRequestException('파일이 존재하지 않습니다.')
         
         return { filePath: file.path }
+    }
 
+    uploadFileForS3(file: Express.MulterS3.File) {
+        console.log(file)
+        if(!file) throw new BadRequestException('파일이 존재하지 않습니다.')
+        
+        return { filePath: file.location }
     }
 }
--- multer_setting.md
+++ multer_setting.md
@@ -1,2 +1,9 @@
 # 파일 저장소를 서버 내에 두는 경우
 
+# S3 서버에 저장소를 두는 경우
+
+```sh
+$ npm i @aws-sdk/client-s3
+$ npm i multer-s3
+$ npm i -D @types/multer-s3
+```
